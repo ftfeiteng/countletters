55ad6704fc9f867fb8d8828179204be495114f60
Tengf <ftfeiteng@gmail.com>
2018-11-09 11:49:31 +1100

    Show progarm's name in console

--- src/com/example/main/Main.java
+++ src/com/example/main/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Input your text");
+		System.out.println("LetterCounter\nPlease input your text:");
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String text = null;
